8aa449685c2ab817db8418202b3ba7670c6e4134
Jaspreet Singh <jaspreetsingh145@gmail.com>
2018-03-16 00:33:07 +0530

    cleaner and debugged

--- project.c
+++ project.c
@@ -1,35 +1,40 @@
 #include<stdio.h>
 #include<unistd.h>
 int n;
+//ta and wt are turnaround and waiting time 
+//bt and at are burst time and waiting time ;
+//pri is the priority
+int *bt,*at,*pri;
+char **name;
+int * ta,*wt;
+int avgta,avgw;
 int main()
 {
-	
+	int i;
 	printf("enter no of processes:-");
 	scanf("%d",n);
-	pid_t proq1 =(pid_t*)malloc(n*sizeof(pid_t));
-	pid_t proq2 =(pid_t*)malloc(n*sizeof(pid_t));
-	int * ta,*wt,*bt,*at;
+	char buff[10]={0};
+	name=(char *)malloc(n*sizeof(char));
+	for(i=0;i<n;i++)
+	name[i]=malloc(4*sizeof(char));
+	for(i=0;i<n;i++)
+	{
+		itoa(i,buff,10);
+		name[i]=strcat("P",buff);
+	}
+	//this code seems to be at wrong place 
 	ta=(int *)malloc(n*sizeof(int));
 	wt=(int *)malloc(n*sizeof(int));
+	//till here
 	bt=(int *)malloc(n*sizeof(int));
 	at=(int *)malloc(n*sizeof(int));
-	int avgta,avgw;
-	int i;
+	pri=(int *)malloc(n*sizeof(int));
 	for(i=0;i<n;i++)
 	{
 		printf("enter burst time of process %d :-",i+1);
-		scanf("%d",(bt+i));
+		scanf("%d",bt[i]);
 		printf("enter arrival time of process %d :-",i+1);
-		scanf("%d",(at+i);	
+		scanf("%d",at[i]);	
 	}
-	for (i=0;i<n;i++)
-	{
-		*(proq1+i)=fork();
-		switch(proq1)
-		{
-			case 0: kill(*(proq1+i),SIGSTOP);
-					sleep()
-		}
-	}
+	//sorting these on bases of arrival
 }
-int childprocess()
