d07a59dda043b5b5a28fe5ba1ba0923c15850d60
Jaspreet Singh <jaspreetsingh145@gmail.com>
2018-03-20 00:18:36 +0530

    trivial approach

--- project.c
+++ project.c
@@ -1,40 +1,61 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
 int n;
 //ta and wt are turnaround and waiting time 
 //bt and at are burst time and waiting time ;
 //pri is the priority
-int *bt,*at,*pri;
-char **name;
-int * ta,*wt;
+int bt[10],at[10],pri[10];
+int processno[10];
+int ta[10],wt[10];
 int avgta,avgw;
+int isin(int a)
+{
+	int  i;
+	for(i=0;i<n;i++)
+	{
+		if(at[i]==a)
+		{
+			return i;
+		}
+	}
+}
+int min()
+{
+	int a=at[0];
+	int i;
+	for(i=0;i<n;i++)
+	{
+		if(a[i]<a)
+		{
+			j=i;
+		}
+	}
+	return j;
+}
 int main()
 {
 	int i;
 	printf("enter no of processes:-");
-	scanf("%d",n);
-	char buff[10]={0};
-	name=(char *)malloc(n*sizeof(char));
-	for(i=0;i<n;i++)
-	name[i]=malloc(4*sizeof(char));
+	scanf("%d",&n);
+	int rt[n];
 	for(i=0;i<n;i++)
 	{
-		itoa(i,buff,10);
-		name[i]=strcat("P",buff);
+		processno[i]=i;
 	}
-	//this code seems to be at wrong place 
-	ta=(int *)malloc(n*sizeof(int));
-	wt=(int *)malloc(n*sizeof(int));
-	//till here
-	bt=(int *)malloc(n*sizeof(int));
-	at=(int *)malloc(n*sizeof(int));
-	pri=(int *)malloc(n*sizeof(int));
 	for(i=0;i<n;i++)
 	{
 		printf("enter burst time of process %d :-",i+1);
-		scanf("%d",bt[i]);
+		scanf("%d",&bt[i]);
+		rt[i]=bt[i];
 		printf("enter arrival time of process %d :-",i+1);
-		scanf("%d",at[i]);	
+		scanf("%d",&at[i]);	
+		printf("enter priority of process %d :-",i+1);
+		scanf("%d",&pri[i]);	
 	}
-	//sorting these on bases of arrival
+	int timeline=0;
+	int remaining = n;
+	//what if two processes arrive at same time 
+	//what should be the loop
 }
+
