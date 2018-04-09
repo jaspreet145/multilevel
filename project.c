aabe0f1db675e3e324ea96759a601b8002578ab8
Jaspreet Singh <jaspreetsingh145@gmail.com>
2018-04-09 19:17:27 +0530

    final submission

--- project.c
+++ project.c
@@ -6,6 +6,7 @@ int n;
 //ta and wt are turnaround and waiting time 
 //bt and at are burst time and waiting time ;
 //pri is the priority
+//only for max 10 processes
 int bt[10],at[10],pri[10];
 int processno[10];
 int ta[10],wt[10],flag[10]={0},exec[10];
@@ -22,8 +23,17 @@ int display()
 	printf("\nAverage Waiting Time= %f\n",wait*1.0/n); 
   printf("Avg Turnaround Time = %f",turn*1.0/n); 
 }
-void
- prisort(int n)		//this function is correct
+int idel(int checker[],int i)
+{
+	int j=0;
+	for(j=0;j<i;j++)
+	{
+		if(exec[checker[j]]==0)
+			break;
+	}
+	return j;
+}
+void prisort()		//this function is correct
 {
 	int i=0,j=0,k=n-1,max;
 	while(j<n)
@@ -74,17 +84,17 @@ int isin(int c,int b)			//this is correct
 	{
 		for(j=0;j<n;j++)
 	{
-		if(at[j]==a)
+		if(at[j]==c)
 		{
 			return j;
 		
 		}
 	}
-}
+	
+	}
 	return i;
 }
-
-int min(int time,int n)    //this function is correct
+int min(int time)    //this function is correct
 {
 	int i,j=-1;
 	for(i=0;i<n;i++)
@@ -104,7 +114,7 @@ int min(int time,int n)    //this function is correct
 }
 int main()
 {
-	int i=0,j,temp,temp1=0;
+	int i=0,j,temp,temp1;
 	printf("enter no of processes:-");
 	scanf("%d",&n);
 	int rt[n];
@@ -128,29 +138,29 @@ int main()
 	int remaining = n;
 	int checker[10];
 	int k=0;
-	prisort(n);
+	prisort();
 	int index;
+
 	for(i=0;i<n;i++)
 		flag[i]=0;
 	i=0;
 	while(remaining!=0)
 	{
-		printf("%d\n",remaining);
-		while((k = min(timeline,n))!=-1)
+		while((k = min(timeline))!=-1)
 		{
 			checker[i]=k;
 			i++;
-		
 		}
-		if(i==0)
+		if(i==0 || idel(checker,i)==i)
 		{
 			timeline++;
 			continue;
 		}
+
 		int index= INT_MAX;
-		for(j=0;j<i;j++)			//this is correct
+	for(j=0;j<i;j++)			//this is correct
 		{
-			if(exec[j]!=1)
+			if(exec[checker[j]]!=1)
 				{
 			for(k=0;k<n;k++)
 			{
@@ -165,31 +175,87 @@ int main()
 						}
 					}
 				}
-				
 			
-			}else
-					continue;
+			}
 		}
-
-		temp1=a[index];	
+		if(index==INT_MAX)
+			continue;
+		else{
+			temp1=a[index];	
 		index=temp1;
-		temp=rt[index];		//think on this
-			
-//change here
-	for(j=0;j<=temp && (isin(timeline,index))==-1&& rt[index]>0;j++)   //change in this line
+		temp=rt[index];
+		}
+	
+		for(j=0;j<=temp&&(isin(timeline,index))==-1&& rt[index]>0;j++)   //j replaced with timeline //check this
 		{
 			rt[index]--;
 			timeline++;
 		}
+		if(rt[index]>0)
+		{
+				while((k = min(timeline))!=-1)
+				{
+					checker[i]=k;
+					i++;
+					while(pri[k]>pri[index]&&isin(timeline,k)==-1&&rt[index]!=0)
+					{	rt[index]--;
+						timeline++;
+						if(isin(timeline,-1)!=-1)
+							break;
+					}
+				}
+			
+				
+			if(pri[isin(timeline,-1)]<pri[index]&&isin(timeline,-1)!=-1)		//this is correct
+			{
+
+				queue2[q2size]=index;
+				q2size++;
+				remaining--;
+				exec[index]=1;
+			}
+		}
 		if(rt[index]==0)	//this is correct
 		{
-			wt[index]=wait+timeline-at[index]-bt[index];   
+			wt[index]=timeline-at[index]-bt[index];   
 			wait=wait+wt[index];
 			ta[index]=timeline-at[index];
 			turn=turn+ta[index];
 			exec[index]=1;
 			remaining--;
 		}	
+		
+	}
+	//after this it might be wrong
+	int timeq=2;
+	remaining = q2size;
+	i=0;
+	int check[10]={0};
+	while(remaining!=0)
+	{
+		 if(rt[queue2[i]]<=timeq && rt[queue2[i]]>0) 
+    { 
+      timeline+=rt[queue2[i]]; 
+      rt[queue2[i]]=0;  
+    } 
+    else if(rt[queue2[i]]>0)
+    { 
+      rt[queue2[i]]-=timeq; 
+      timeline+=timeq; 
+    } 
+    if(rt[queue2[i]]==0&&check[queue2[i]]==0) 
+    { 							
+      remaining--;  
+     	wt[queue2[i]]+=timeline-at[queue2[i]]-bt[queue2[i]];   
+		wait=wait+wt[queue2[i]];
+		ta[queue2[i]]+=timeline-at[queue2[i]];
+		turn=turn+ta[queue2[i]];
+		check[queue2[i]]=1; 
+    } 
+    if(i==q2size-1)
+      i=0;  
+    else 			
+      i++; 
 	}
 	display();
 }
